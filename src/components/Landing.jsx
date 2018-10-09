6b13009c20c054ee8bb9700b7ece6e83752b6214
unknown <jtray@outlook.es>
2018-10-08 22:49:36 -0600

    landing completed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
-class Landing extends Component{
-    render(){
-        return(
-            <div>
-                Landing
+import Links from './Links';
+import headshot from '../Images/selfie3.png'
+class Landing extends Component {
+    render() {
+        return (
+            <div className='Landing'>
+                <img className='headshot' src={headshot} alt="Tyler Headshot" />
+                <h1 id='name'>J. Tyler Ray</h1>
+                <div className="sub-wrapper">
+                    <h4>Front End</h4>
+                    <h4>Back End</h4>
+                    <h4>Full Stack</h4>
+                    <h4>Endpoint Testing</h4>
+                </div>
+                <Links />
             </div>
         )
     }
